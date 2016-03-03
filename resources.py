1e1574d8c711b4b03c0e1d941a56303caebe7d9e
Andy Aschwanden <aaschwanden@alaska.edu>
2016-03-02 17:31:33 -0900

    fixed some typos

--- resources.py
+++ resources.py
@@ -412,9 +412,9 @@ cd $SLURM_SUBMIT_DIR
 # Generate a list of compute node hostnames reserved for this job,
 # this ./nodes file is necessary for slurm to spawn mpi processes
 # across multiple compute nodes
-srun -l /bin/hostname | sort -n | awk \'{{print $2}}\' > ./nodes_$SLURM_JOB_ID
+srun -l /bin/hostname | sort -n | awk \'{{print $2}}\' > ./nodes_$SLURM_JOBID
 
-{mpido} {cores} -machinefile ./nodes """.format(queue=queue, walltime=walltime, nodes=nodes, ppn=ppn, cores=cores, mpido=systems[system]['mpido'])
+{mpido} {cores} -machinefile ./nodes_$SLURM_JOBID """.format(queue=queue, walltime=walltime, nodes=nodes, ppn=ppn, cores=cores, mpido=systems[system]['mpido'])
     elif system in ('pleiades'):
         
         header = """#PBS -S /bin/bash
