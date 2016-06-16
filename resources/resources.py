101fbe8ba3cedc16e5b004d3d37927000459616d
Andy Aschwanden <aaschwanden@alaska.edu>
2016-06-15 20:49:25 -0800

    added ssafd_ prefix

--- resources/resources.py
+++ resources/resources.py
@@ -384,8 +384,8 @@ def generate_stress_balance(stress_balance, additional_params_dict):
     params_dict['stress_balance'] = stress_balance
     if stress_balance in ('ssa+sia'):
         params_dict['options_left'] = ''
-        params_dict['pc_type'] = 'asm'
-        params_dict['sub_pc_type'] = 'jacobi'
+        params_dict['ssafd_pc_type'] = 'asm'
+        params_dict['ssafd_sub_pc_type'] = 'lu'
         params_dict['cfbc'] = ''
         params_dict['kill_icebergs'] = ''
         params_dict['part_grid'] = ''
