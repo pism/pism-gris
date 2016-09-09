f6a216641233bf7badeb42226df7fe0492c2da31
Andy Aschwanden <aaschwanden@alaska.edu>
2016-09-09 13:07:19 -0800

    added snap shot dict

--- initialization/init_restart.py
+++ initialization/init_restart.py
@@ -244,7 +244,7 @@ for n, combination in enumerate(combinations):
                                                 odir=odir)
             snap_shot_dict = generate_snap_shots(outfile, save_times, odir=odir)
 
-            all_params_dict = merge_dicts(general_params_dict, grid_params_dict, stress_balance_params_dict, climate_params_dict, ocean_params_dict, hydro_params_dict, calving_params_dict, spatial_ts_dict, scalar_ts_dict)
+            all_params_dict = merge_dicts(general_params_dict, grid_params_dict, stress_balance_params_dict, climate_params_dict, ocean_params_dict, hydro_params_dict, calving_params_dict, spatial_ts_dict, scalar_ts_dict, snap_shot_dict)
             all_params = ' '.join([' '.join(['-' + k, str(v)]) for k, v in all_params_dict.items()])
 
             cmd = ' '.join([batch_system['mpido'], prefix, all_params, '> {outdir}/job.${batch}  2>&1'.format(outdir=odir,batch=batch_system['job_id'])])
