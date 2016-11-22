fb4bacb9f6bd3ccef02807d35c74eb888cba9533
Andy Aschwanden <andy.aschwanden@gmail.com>
2016-11-22 14:49:14 -0800

    added missing units to topg_delta

--- initialization/create_topg_delta_file.sh
+++ initialization/create_topg_delta_file.sh
@@ -10,4 +10,4 @@ ncks -O -v bed $orig_file $delta_file
 ncks -A -v topg $adjusted_file $delta_file
 ncap2 -O -s "topg_delta=topg-float(bed);" $delta_file $delta_file
 ncks -O -x -v topg,bed $delta_file $delta_file
-ncatted -a standard_name,topg_delta,d,, -a long_name,topg_delta,o,c,"topg_adjusted-topg_original" -a pism_intent,topg_delta,d,, $delta_file
+ncatted -a units,topg_delta,o,c,"m" -a standard_name,topg_delta,d,, -a long_name,topg_delta,o,c,"topg_adjusted-topg_original" -a pism_intent,topg_delta,d,, $delta_file
