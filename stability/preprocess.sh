40b181a8cffa88b1841ccece0854b26f503c3996
Andy Aschwanden <aaschwanden@alaska.edu>
2017-05-22 10:37:42 +0200

    fixed path

--- stability/preprocess.sh
+++ stability/preprocess.sh
@@ -5,5 +5,5 @@ for tempmax in 0 1 2 3 4 5 6 7 8 9 10; do
 done
 
 for f in 1 1.1 1.25 1.5 2 4; do
-    python ../data_sets/ocean_forcing/create_abrupt_ocean.py -f $f pism_abrupt_ocean_${f}.nc
+    python ../data_sets/ocean_forcing/create_step_ocean.py -f $f pism_abrupt_ocean_${f}.nc
 done
