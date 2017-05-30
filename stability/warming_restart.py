0a44b7bfe5eeb867c5ca5c2c2a9477da3f8bd8e2
Andy Aschwanden <aaschwanden@alaska.edu>
2017-05-30 12:03:33 +0200

    update calibration script to 250m tct

--- stability/warming_restart.py
+++ stability/warming_restart.py
@@ -253,7 +253,7 @@ ocean_melt_power_values = [1]
 if do_tct:
     thickness_calving_threshold_values = [100, 200, 250]
 else:
-    thickness_calving_threshold_values = [100]
+    thickness_calving_threshold_values = [250]
 ppq_values = [0.6]
 tefo_values = [0.020]
 phi_min_values = [5.0]
