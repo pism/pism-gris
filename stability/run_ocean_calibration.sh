29513be1963e985bc69cd0bd89ccf894639c3069
Andy Aschwanden <aaschwanden@alaska.edu>
2017-07-17 13:23:18 -0800

    added adjust_timeline.py to get a timeline that works with the TimeManager

--- stability/run_ocean_calibration.sh
+++ stability/run_ocean_calibration.sh
@@ -49,7 +49,7 @@ tct=low
 
 /Volumes/79n/data/gris-analysis/plotting/plotting.py -o ctrl --plot per_basin_cumulative  --time_bounds 2008 3008 2017_06_ocean_1kyr/spatial_basins/b_*_ex_gris_g2400m_warming_v3a_no_bath_bd_off_calving_vonmises_calving_om_low_tct_low_0_1000/sum_fldsum_b_*_ex_gris_g2400m_warming_v3a_no_bath_bd_off_calving_vonmises_calving_om_${om}_tct_${tct}_0_1000.nc
 
-for rcp in 26 85; do
+for rcp in 26 45 85; do
     end_year=2100
     /Volumes/79n/data/gris-analysis/plotting/plotting.py -o rcp_${rcp}_basin_2008_${end_year} --time_bounds 2008 ${end_year} --bounds -0.05 1.5 --plot basin_mass_d 2017_07_rcps/spatial_basins/b_*_ex_gris_g2400m_warming_v3a_no_bath_rcp_${rcp}_bd_off_calving_vonmises_calving_2008_3008/cumsum_b_*_ex_gris_g2400m_warming_v3a_no_bath_rcp_${rcp}_bd_off_calving_vonmises_calving_2008_3008.nc
     end_year=2200
@@ -59,10 +59,14 @@ for rcp in 26 85; do
     end_year=3000
     /Volumes/79n/data/gris-analysis/plotting/plotting.py -o rcp_${rcp}_basin_2008_${end_year} --time_bounds 2008 ${end_year} --bounds -0.1 8 --plot basin_mass_d 2017_07_rcps/spatial_basins/b_*_ex_gris_g2400m_warming_v3a_no_bath_rcp_${rcp}_bd_off_calving_vonmises_calving_2008_3008/cumsum_b_*_ex_gris_g2400m_warming_v3a_no_bath_rcp_${rcp}_bd_off_calving_vonmises_calving_2008_3008.nc
 
-    # /Volumes/79n/data/gris-analysis/plotting/plotting.py -o rcp_${rcp}_basin --plot basin_d_cumulative 2017_07_rcps/spatial_basins/b_*_ex_gris_g2400m_warming_v3a_no_bath_rcp_${rcp}_bd_off_calving_vonmises_calving_2008_3008/cumsum_b_*_ex_gris_g2400m_warming_v3a_no_bath_rcp_${rcp}_bd_off_calving_vonmises_calving_2008_3008.nc
+    /Volumes/79n/data/gris-analysis/plotting/plotting.py -o rcp_${rcp}_basin --plot basin_d_cumulative 2017_07_rcps/spatial_basins/b_*_ex_gris_g2400m_warming_v3a_no_bath_rcp_${rcp}_bd_off_calving_vonmises_calving_2008_3008/cumsum_b_*_ex_gris_g2400m_warming_v3a_no_bath_rcp_${rcp}_bd_off_calving_vonmises_calving_2008_3008.nc
     /Volumes/79n/data/gris-analysis/plotting/plotting.py -o rcp_${rcp}_basin  --plot per_basin_cumulative 2017_07_rcps/spatial_basins/b_*_ex_gris_g2400m_warming_v3a_no_bath_rcp_${rcp}_bd_off_calving_vonmises_calving_2008_3008/cumsum_b_*_ex_gris_g2400m_warming_v3a_no_bath_rcp_${rcp}_bd_off_calving_vonmises_calving_2008_3008.nc
 done
 
 /Volumes/79n/data/gris-analysis/plotting/plotting.py --plot rcp_mass 2017_07_rcps/scalar/cumsum_ts_gris_g2400m_warming_v3a_no_bath_rcp_*.nc
 /Volumes/79n/data/gris-analysis/plotting/plotting.py --plot rcp_d 2017_07_rcps/scalar/cumsum_ts_gris_g2400m_warming_v3a_no_bath_rcp_*.nc
+
+
+/Volumes/79n/data/gris-analysis/plotting/plotting.py --plot rcp_mass 2017_07_rcps/scalar/cumsum_ts_gris_g3600m_warming_v3a_no_bath_rcp_*.nc
+/Volumes/79n/data/gris-analysis/plotting/plotting.py --plot rcp_d 2017_07_rcps/scalar/cumsum_ts_gris_g3600m_warming_v3a_no_bath_rcp_*.nc
 
