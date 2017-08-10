735d22bfd68e55e0aff8aa882bab73987f9a2f61
Andy Aschwanden <aaschwanden@alaska.edu>
2017-08-10 09:17:17 -0800

    Postprocessing and RCP forcing
    - create anomalies for RCP by subtracting 2006-2015 mean
    - lock I/O with -L for CDO command that would seg fault otherwise

--- data_sets/climate_forcing/prepare_rcp.sh
+++ data_sets/climate_forcing/prepare_rcp.sh
@@ -6,11 +6,12 @@ box="-73,-12,59,84"
 for rcp in 26 45 85; do
     for ens in r1i1p1 r1i1p2 r1i1p3; do
         cdo -O mergetime ${var}_Amon_GISS-E2-H_rcp${rcp}_${ens}_*01-*12.nc  ${var}_Amon_GISS-E2-H_rcp${rcp}_ens_${ens}_2006-2300.nc
-        cdo -O fldmean -sellonlatbox,$box -selyear,2008/2300 ${var}_Amon_GISS-E2-H_rcp${rcp}_ens_${ens}_2006-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ens_${ens}_GRIS_2008-2300.nc
+        cdo -O fldmean -sellonlatbox,$box ${var}_Amon_GISS-E2-H_rcp${rcp}_ens_${ens}_2006-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ens_${ens}_GRIS_2006-2300.nc
     done
-    cdo -O ensmean ${var}_Amon_GISS-E2-H_rcp${rcp}_ens_*_GRIS_2008-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_GRIS_2008-2300.nc
-    cdo -O yearmean ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_GRIS_2008-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_GRIS_2008-2300.nc
-    cdo -O sub ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_GRIS_2008-2300.nc -seltimestep,1 ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_GRIS_2008-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2008-2300.nc
+    cdo -O ensmean ${var}_Amon_GISS-E2-H_rcp${rcp}_ens_*_GRIS_2006-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_GRIS_2006-2300.nc
+    cdo -O yearmean ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_GRIS_2006-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_GRIS_2006-2300.nc
+    cdo -O sub ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_GRIS_2006-2300.nc -timmean -selyear,2006/2015  ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_GRIS_2006-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2006-2300.nc
+    cdo selyear,2008/2300 ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2006-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2008-2300.nc
     cdo -O trend -selyear,2201/2300 ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2008-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2008-2300_a.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2008-2300_b.nc
     ncks -O ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2008-2300.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2008-7008.nc
     python extend_cmip5.py ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2008-7008.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2008-2300_a.nc ${var}_Amon_GISS-E2-H_rcp${rcp}_ensmean_ym_anom_GRIS_2008-2300_b.nc
