cf0d7e1d1172dfb9180a83cb9f2701f26d44561c
Andy Aschwanden <aaschwanden@alaska.edu>
2017-11-29 17:23:03 -0900

    temporary fix for run script

--- stability/run_les.sh
+++ stability/run_les.sh
@@ -74,7 +74,7 @@ mkdir -p $odir/final_states
 cd $odir/state
 for file in gris_g${grid}m*0_1000.nc; do
     cdo aexpr,usurf=topg+thk -selvar,topg,thk,velsurf_mag $file ../final_states/$file
-    ncap2 -O -s "where(thk<10) {velsurf_mag=-2e9; usurf=0.;};" ../final_states/$file ../final_states/$file
+    ncap2 -O -s "where(topg<0) {topg=0.;}; where(thk<10) {velsurf_mag=-2e9; usurf=0.;};" ../final_states/$file ../final_states/$file
     gdal_translate NETCDF:../final_states/$file:velsurf_mag ../final_states/velsurf_mag_$file.tif
     gdal_translate -a_nodata 0 NETCDF:../final_states/$file:usurf ../final_states/usurf_$file.tif
     gdaldem hillshade ../final_states/usurf_$file.tif ../final_states/hs_usurf_$file.tif
@@ -266,7 +266,7 @@ grid=1800
 
 ./lhs_ensemble.py -e ../latin_hypercube/lhs_control.csv --o_dir ${odir} --exstep 1 -n ${n} -w 36:00:00 -g ${grid} -s ${s} -q ${q} --step 1000 --duration 1000 ../calibration/2017_06_vc/state/gris_g${grid}m_flux_v3a_no_bath_sia_e_1.25_sia_n_3_ssa_n_3.25_ppq_0.6_tefo_0.02_calving_vonmises_calving_0_100.nc
 
-odir=2017_11_ctrl
+odir=2017_12_ctrl
 s=chinook
 q=t2standard
 n=360
