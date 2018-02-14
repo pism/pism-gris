0d3482bf1339fb4196b18ebb18ec44b5304f3fdc
Andy Aschwanden <aaschwanden@alaska.edu>
2018-02-14 14:17:42 -0900

    shorter backup intervals and minor stuff

--- stability/post_les.sh
+++ stability/post_les.sh
@@ -14,12 +14,12 @@ ulimit
 
 set +e
 
-odir=2017_12_les
+odir=2018_01_les
 mkdir -p $odir/dgmsl
-for grid in 3600; do
+for grid in 1800; do
     for rcp in 26 45 85; do
         for year in 2100 2200 3000; do
-            for id2 in `seq 0 9`; do
+            for id2 in `seq 0 4`; do
                 for id1 in `seq 0 9`; do
                     for id in `seq 0 9`; do
                         cdo -L mulc,-1000 -divc,365 -divc,1e15 -selvar,limnsw -sub -selyear,$year $odir/scalar_pruned/ts_gris_g${grid}m_v3a_rcp_${rcp}_id_${id2}${id1}${id}_0_1000.nc -selyear,2008 $odir/scalar_pruned/ts_gris_g${grid}m_v3a_rcp_${rcp}_id_${id2}${id1}${id}_0_1000.nc $odir/dgmsl/dgms_g${grid}m_rcp_${rcp}_id_${id2}${id1}${id}_${year}.nc
--- stability/run_les.sh
+++ stability/run_les.sh
@@ -188,27 +188,28 @@ mkdir -p ${odir}/station_ts
 mkdir -p ${odir}/profiles
 for rcp in 26 45 85; do
     # extract_profiles.py -v thk,usurf,tempsurf ../../data_sets/GreenlandIceCoreSites/ice-core-sites.shp ${odir}/spatial/ex_g${grid}m_v3a_rcp_${rcp}_id_CTRL_0_3000.nc ${odir}/station_ts/profile_g${grid}m_v3a_rcp_${rcp}_id_CTRL_0_3000.nc
-    extract_profiles.py -v velsurf_mag,thk,usurf,topg,beta,mask ../../gris-outlet-glacier-profiles/gris-outlet-glacier-profiles-100m.shp 2017_12_ctrl/spatial/ex_g900m_v3a_rcp_${rcp}_id_CTRL_0_3000.nc 2017_12_ctrl/profiles/profiles_100m_ex_g900m_v3a_rcp_${rcp}_id_CTRL_0_3000.nc
+    extract_profiles.py -v velsurf_mag,velbase_mag,thk,usurf,topg ../../gris-outlet-glacier-profiles/gris-outlet-glacier-profiles-100m.shp 2017_12_ctrl/spatial/ex_g900m_v3a_rcp_${rcp}_id_CTRL_0_3000.nc 2017_12_ctrl/profiles/profiles_100m_ex_g900m_v3a_rcp_${rcp}_id_CTRL_0_3000.nc
 done
 
 
 # CMIP forcing
 ~/base/gris-analysis/plotting/plotting.py  -n 8 -o giss --plot cmip5 --time_bounds 2008 3000 ../data_sets/climate_forcing/tas_Amon_GISS-E2-H_rcp*0-5000.nc ../data_sets/climate_forcing/tas_cmip5_rcp*ensstd**anom*.nc
-
 # Cumulative contribution LES and CTRL
 ~/base/gris-analysis/plotting/plotting.py  -n 8 -o les --time_bounds 2008 3000 --ctrl_file 2017_12_ctrl/scalar/ts_gris_g900m_v3a_rcp_*_id_CTRL_0_1000.nc --plot rcp_mass 2017_12_les/scalar_ensstat/ens*_0_1000.nc
+~/base/gris-analysis/plotting/plotting.py  -n 8 -o les18 --time_bounds 2008 3000 --ctrl_file 2017_12_ctrl/scalar/ts_gris_g900m_v3a_rcp_*_id_CTRL_0_1000.nc --plot rcp_mass 2018_01_les/scalar_ensstat/ens*_0_1000.nc
 # Rates of GMSL rise LES and CTRL
 ~/base/gris-analysis/plotting/plotting.py -n 8 -o les --time_bounds 2008 3000 --no_legend --ctrl_file 2017_12_ctrl/scalar/ts_gris_g900m_v3a_rcp_*_id_CTRL_0_1000.nc --plot rcp_flux 2017_12_les/scalar_ensstat/ens*_0_1000.nc
+~/base/gris-analysis/plotting/plotting.py -n 8 -o les18 --time_bounds 2008 3000 --no_legend --ctrl_file 2017_12_ctrl/scalar/ts_gris_g900m_v3a_rcp_*_id_CTRL_0_1000.nc --plot rcp_flux 2018_01_les/scalar_ensstat/ens*_0_1000.nc
 # Long term evolution
 ~/base/gris-analysis/plotting/plotting.py  -o ctrl5k --plot ctrl_mass --time_bounds 2008 5000  2017_12_ctrl/scalar/ts_gris_g900m_v3a_rcp_*_id_CTRL_0_3000.nc 2017_12_ctrl/scalar/ts_gris_g900m_v3a_rcp_85_id_CTRL_0_2000.nc
 
 # Profiles
-~/base/gris-analysis/plotting/plotting.py --bounds 0 12000 --time_bounds 2009 2300  -o rcp26 --plot profile_combined 2017_12_ctrl/profiles/profiles_100m_ex_g900m_v3a_rcp_26_id_CTRL_0_3000.nc
-~/base/gris-analysis/plotting/plotting.py --bounds 0 12000 --time_bounds 2009 2300  -o rcp45 --plot profile_combined 2017_12_ctrl/profiles/profiles_100m_ex_g900m_v3a_rcp_45_id_CTRL_0_3000.nc
-~/base/gris-analysis/plotting/plotting.py --bounds 0 12000 --time_bounds 2009 2300  -o rcp85 --plot profile_combined 2017_12_ctrl/profiles/profiles_100m_ex_g900m_v3a_rcp_85_id_CTRL_0_3000.nc
+~/base/gris-analysis/plotting/plotting.py --bounds 0 12000 --time_bounds 2015 2315  -o rcp26 --plot profile_combined 2017_12_ctrl/profiles/profiles_100m_ex_g900m_v3a_rcp_26_id_CTRL_0_3000.nc
+~/base/gris-analysis/plotting/plotting.py --bounds 0 12000 --time_bounds 2015 2315  -o rcp45 --plot profile_combined 2017_12_ctrl/profiles/profiles_100m_ex_g900m_v3a_rcp_45_id_CTRL_0_3000.nc
+~/base/gris-analysis/plotting/plotting.py --bounds 0 12000 --time_bounds 2015 2315  -o rcp85 --plot profile_combined 2017_12_ctrl/profiles/profiles_100m_ex_g900m_v3a_rcp_85_id_CTRL_0_3000.nc
 
 # Flux Partitioning
-~/base/gris-analysis/plotting/plotting.py -n 4 -o ctrl --time_bounds 2008 3000 --no_legend --plot flux_partitioning 2017_12_ctrl/scalar/ts_gris_g900m_v3a_rcp_*id_CTRL_0_1000.nc
+~/base/gris-analysis/plotting/plotting.py -n 4 -o ctrl --time_bounds 2008 3000 --no_legend --plot flux_partitioning 2017_12_ctrl/scalar/ts_gris_g900m_v3a_rcp_*id_CTRL_0_1000.nc 2017_12_ctrl/scalar/ts_gris_g3600m_v3a_rcp_*id_NTRL_0_1000.nc
 # Basin Flux Partitioning
 ~/base/gris-analysis/plotting/plotting.py -n 4 -o ctrl --bounds -850 450 --time_bounds 2008 3000 --no_legend --plot basin_flux_partitioning 2017_12_ctrl/basins/scalar/ts_b_*_ex_g900m_v3a_rcp_*_id_CTRL_0_2000.nc
 
@@ -220,8 +221,6 @@ done
 ~/base/gris-analysis/plotting/plotting.py -o ctrl --time_bounds 2008 3000 --no_legend --plot station_usurf 2017_12_ctrl/station_ts/profile_g900m_v3a_rcp_*_id_CTRL_0_1000.nc
 
 ~/base/gris-analysis/plotting/plotting.py -o ctrl --time_bounds 2008 3000 --no_legend --plot per_basin_flux 2017_12_ctrl/basins/scalar/ts_b_*_ex_g900m_v3a_rcp_*_id_CTRL_0_2000.nc
-
-# CMIP5
 
 
 # grid resolution
