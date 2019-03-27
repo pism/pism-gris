01a48d59756d564d1ab8106ea1d104f8d2bcce39
Andy Aschwanden <aaschwanden@alaska.edu>
2019-03-27 07:07:41 -0800

    added documentation to draw samples

--- sobol/draw_samples.py
+++ sobol/draw_samples.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+# (C) 2017-2019 Andy Aschwanden, Doug Brinkerhoff
+
+# This script draws samples with the Sobol Sequences
+# using the Saltelli method
+#
+# Herman, J., Usher, W., (2017), SALib:
+# An open-source Python library for Sensitivity Analysis, Journal of Open Source Software,
+# 2(9), 97, doi:10.21105/joss.00097
 
 from argparse import ArgumentParser
 import numpy as np
@@ -31,24 +39,27 @@ distributions = {
     "SIAE": gamma(1.5, scale=0.8, loc=1),
 }
 
+# Generate the Sobol sequence samples with uniform distributions
+
+
 # Names of all the variables
 keys = [x for x in distributions.keys()]
 
-# Generate the Sobol sequence samples with uniform distributions
-
+# Describe the Problem
 problem = {"num_vars": len(keys), "names": keys, "bounds": [[0, 1]] * len(keys)}
 
-# Generate samples
+# Generate uniform samples (i.e. one unit hypercube)
 unif_sample = saltelli.sample(problem, n_samples, calc_second_order=False)
 
 # To hold the transformed variables
 dist_sample = np.zeros_like(unif_sample)
 
-
+# Now transform the unit hypercube to the prescribed distributions
 # For each variable, transform with the inverse of the CDF (inv(CDF)=ppf)
 for i, key in enumerate(keys):
     dist_sample[:, i] = distributions[key].ppf(unif_sample[:, i])
 
+# Save to CSV file using Pandas DataFrame and to_csv method
 header = keys
 # Convert to Pandas dataframe, append column headers, output as csv
 df = pd.DataFrame(data=dist_sample, columns=header)
