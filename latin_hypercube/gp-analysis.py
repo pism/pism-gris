cc32d420f92bcbce06319d320d735d9704d67c5a
Andy Aschwanden <aaschwanden@alaska.edu>
2019-06-16 10:06:33 -0800

    work on GP analysis

--- latin_hypercube/gp-analysis.py
+++ latin_hypercube/gp-analysis.py
@@ -68,23 +68,7 @@ def draw_samples(n_samples=1000):
     return pd.DataFrame(data=dist_sample, columns=header)
 
 
-rcp_col_dict = {"CTRL": "k", "85": "#990002", "45": "#5492CD", "26": "#003466"}
-rcp_shade_col_dict = {"CTRL": "k", "85": "#F4A582", "45": "#92C5DE", "26": "#4393C3"}
-
-
-# The lines below are specific to the notebook format
-matplotlib.rcParams["figure.figsize"] = (12, 6)
-
-# Load Samples file as Pandas DataFrame
-samples_file = "2018_09_les/lhs_samples_gcm.csv"
-samples = pd.read_csv(samples_file, delimiter=",", squeeze=True, skipinitialspace=True)
-
-pctls, pctls_gp = [], []
-
-rcp = "85"
-
-
-def analyze(year):
+def gp_emulate(year):
 
     # Load respone file as Pandas DataFrame
     response_file = "2018_09_les/dgmsl_csv/dgmsl_rcp_{}_{}.csv".format(rcp, year)
@@ -111,16 +95,95 @@ def analyze(year):
     m = gp.models.GPRegression(X, Y, k)
     m.optimize(messages=True)
 
-    X_new = draw_samples(5000)
+    X_new = draw_samples(10000)
 
     p = m.predict(X_new.values)
 
-    pctls.append(np.percentile(Y, [5, 16, 50, 84, 95]))
-    pctls_gp.append(np.percentile(p[0], [5, 16, 50, 84, 95]))
+    pctls_gp = np.percentile(p[0], m_percentiles)
+    pctls = np.percentile(Y, m_percentiles)
+    pctls_df = pd.DataFrame(data=np.vstack([pctls, pctls_gp]).T, index=[5, 16, 50, 84, 95], columns=["lhs", "gp"])
+    pctls_df.to_csv("gp_pctl_rcp_{}_{}.csv".format(rcp, year), index_label="percentile")
 
 
-p = Pool(4)
-p.map(analyze, range(10))
+def gp_loo(kern, year):
+
+    # Load respone file as Pandas DataFrame
+    response_file = "2018_09_les/dgmsl_csv/dgmsl_rcp_{}_{}.csv".format(rcp, year)
+    response = pd.read_csv(response_file, delimiter=",", squeeze=True, skipinitialspace=True)
+    # It is possible that not all ensemble simulations succeeded and returned a value
+    # so we much search for missing response values
+    missing_ids = list(set(samples["id"]).difference(response["id"]))
+    Y = -response[response.columns[-1]].values.reshape(1, -1).T
+    if missing_ids:
+        print("The following simulation ids are missing:\n   {}".format(missing_ids))
+        # and remove the missing samples
+        samples_missing_removed = samples[~samples["id"].isin(missing_ids)]
+        X = samples_missing_removed.values[:, 1::]
+
+    else:
+        X = samples.values[:, 1::]
+
+    pool = Pool(4)
+
+    n = X.shape[1]
+    k = gp.kern.Exponential(input_dim=n - 1, ARD=True)
+    pool.map(gp_loo_mp, range(10))
+
+
+def gp_loo_mp(loo_idx):
+
+    X_loo = np.delete(X, loo_idx, axis=0)
+    Y_loo = np.delete(Y, loo_idx, axis=0)
+
+    X_predict = X[loo_idx, :].reshape(1, -1)
+
+    # Dimension n of kernel
+    n = X_loo.shape[1]
+
+    # We choose a kernel
+    print(kern)
+    # k = kern(input_dim=n, ARD=True)
+
+    m = gp.models.GPRegression(X_loo, Y_loo, k)
+    m.optimize(messages=True)
+
+
+rcp_col_dict = {"CTRL": "k", "85": "#990002", "45": "#5492CD", "26": "#003466"}
+rcp_shade_col_dict = {"CTRL": "k", "85": "#F4A582", "45": "#92C5DE", "26": "#4393C3"}
+
+# Load Samples file as Pandas DataFrame
+samples_file = "2018_09_les/lhs_samples_gcm.csv"
+samples = pd.read_csv(samples_file, delimiter=",", squeeze=True, skipinitialspace=True)
+
+rcp = "85"
+m_percentiles = [5, 16, 50, 84, 95]
+
+# pool = Pool(4)
+# pool.map(gp_emulate, range(292))
+
+# gp_loo(gp.models.GPRegression, 92)
+
+date = np.arange(0, 292) + 2009
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+pctls_gp, pctls_lhs = [], []
+for idx, pctl in enumerate(m_percentiles):
+    pctls_lhs.append([])
+    pctls_gp.append([])
+    for year in range(len(date)):
+        df = pd.read_csv("gp_pctl_rcp_{}_{}.csv".format(rcp, year))
+        pctls_gp[idx].append(df[df["percentile"] == pctl]["gp"].values[0])
+        pctls_lhs[idx].append(df[df["percentile"] == pctl]["lhs"].values[0])
+ax.fill_between(date, pctls_gp[0], pctls_gp[4], color=rcp_col_dict[rcp], linewidth=0.3, linestyle="-", alpha=0.1)
+ax.fill_between(date, pctls_gp[1], pctls_gp[3], color=rcp_col_dict[rcp], linewidth=0.3, linestyle="-", alpha=0.3)
+ax.plot(date, np.asarray(pctls_gp).T, color=rcp_col_dict[rcp], linewidth=0.3, linestyle="-")
+ax.plot(date, np.asarray(pctls_lhs).T, color=rcp_col_dict[rcp], linewidth=0.3, linestyle=":")
+ax.set_ylabel("Sea-level equivalent (cm)")
+ax.set_xlabel("Year")
+ax.set_xlim(2008, 2300)
+set_size(3.2, 1)
+fig.savefig("gp_rcp_{}.png".format(rcp), bbox_inches="tight", dpi=600)
 
 
 # fig = plt.figure()
@@ -144,28 +207,28 @@ p.map(analyze, range(10))
 # set_size(3.2, 1.2)
 # fig.savefig("gp_rcp_{}_hist_2100.png".format(rcp), bbox_inches="tight", dpi=600)
 
-# Extract percentiles
-pctl5_gp = [x[0] for x in pctls_gp]
-pctl16_gp = [x[1] for x in pctls_gp]
-pctl50_gp = [x[2] for x in pctls_gp]
-pctl84_gp = [x[3] for x in pctls_gp]
-pctl95_gp = [x[4] for x in pctls_gp]
+# # Extract percentiles
+# pctl5_gp = [x[0] for x in pctls_gp]
+# pctl16_gp = [x[1] for x in pctls_gp]
+# pctl50_gp = [x[2] for x in pctls_gp]
+# pctl84_gp = [x[3] for x in pctls_gp]
+# pctl95_gp = [x[4] for x in pctls_gp]
 
-lw = 0.2
-date = np.arange(0, 92) + 2008
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.fill_between(date, pctl5_gp, pctl95_gp, color=rcp_col_dict[rcp], linewidth=lw, alpha=0.1)
-ax.fill_between(date, pctl16_gp, pctl84_gp, color=rcp_col_dict[rcp], linewidth=lw, alpha=0.2)
-ax.plot(date, pctls, color=rcp_col_dict[rcp], linewidth=0.3, linestyle=":")
-ax.plot(date, pctls_gp, color=rcp_col_dict[rcp], linewidth=0.3, linestyle="-")
-ax.plot(date, pctl50, color=rcp_col_dict[rcp], linewidth=0.6, linestyle=":")
-ax.plot(date, pctl50_gp, color=rcp_col_dict[rcp], linewidth=0.6, linestyle="-")
-ax.set_ylabel("Sea-level equivalent (cm)")
-ax.set_xlabel("Year")
-ax.set_xlim(2008, 2100)
-set_size(3.2, 1)
-fig.savefig("gp_rcp_{}.png".format(rcp), bbox_inches="tight", dpi=600)
+# lw = 0.2
+# date = np.arange(0, 92) + 2008
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.fill_between(date, pctl5_gp, pctl95_gp, color=rcp_col_dict[rcp], linewidth=lw, alpha=0.1)
+# ax.fill_between(date, pctl16_gp, pctl84_gp, color=rcp_col_dict[rcp], linewidth=lw, alpha=0.2)
+# ax.plot(date, pctls, color=rcp_col_dict[rcp], linewidth=0.3, linestyle=":")
+# ax.plot(date, pctls_gp, color=rcp_col_dict[rcp], linewidth=0.3, linestyle="-")
+# ax.plot(date, pctl50, color=rcp_col_dict[rcp], linewidth=0.6, linestyle=":")
+# ax.plot(date, pctl50_gp, color=rcp_col_dict[rcp], linewidth=0.6, linestyle="-")
+# ax.set_ylabel("Sea-level equivalent (cm)")
+# ax.set_xlabel("Year")
+# ax.set_xlim(2008, 2100)
+# set_size(3.2, 1)
+# fig.savefig("gp_rcp_{}.png".format(rcp), bbox_inches="tight", dpi=600)
 
 # p = []
 # s = []
