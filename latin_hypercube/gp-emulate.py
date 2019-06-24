1ba43ef862345d6d31ea939548e21f7776e67a3f
Andy Aschwanden <aaschwanden@alaska.edu>
2019-06-23 18:50:07 -0800

    work on saving results to a netcdf file

--- latin_hypercube/gp-emulate.py
+++ latin_hypercube/gp-emulate.py
@@ -1,6 +1,9 @@
 #!/env/bin python
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from dateutil import rrule
+from dateutil.parser import parse
+from datetime import datetime
 
 from functools import partial
 
@@ -14,6 +17,8 @@ multiprocessing.set_start_method("forkserver", force=True)
 from multiprocessing import Pool
 
 from netCDF4 import Dataset as NC
+from cftime import utime
+
 import numpy as np
 import pandas as pd
 import pylab as plt
@@ -25,6 +30,122 @@ import re
 import os
 
 
+def create_netcdf(
+    filename,
+    start_date="2008-1-1",
+    end_date="2300-1-1",
+    ref_unit="days",
+    ref_date="2008-1-1",
+    periodicity="yearly",
+    interval_type="mid",
+):
+
+    nc = NC(filename, "w")
+
+    time_units = "%s since %s" % (ref_unit, ref_date)
+    # currently PISM only supports the gregorian standard calendar
+    # once this changes, calendar should become a command-line option
+    time_calendar = "standard"
+
+    cdftime = utime(time_units, time_calendar)
+
+    # create a dictionary so that we can supply the periodicity as a
+    # command-line argument.
+    pdict = {}
+    pdict["SECONDLY"] = rrule.SECONDLY
+    pdict["MINUTELY"] = rrule.MINUTELY
+    pdict["HOURLY"] = rrule.HOURLY
+    pdict["DAILY"] = rrule.DAILY
+    pdict["WEEKLY"] = rrule.WEEKLY
+    pdict["MONTHLY"] = rrule.MONTHLY
+    pdict["YEARLY"] = rrule.YEARLY
+    prule = pdict[periodicity.upper()]
+
+    # reference date from command-line argument
+    r = time_units.split(" ")[2].split("-")
+    refdate = datetime(int(r[0]), int(r[1]), int(r[2]))
+
+    # create list with dates from start_date until end_date with
+    # periodicity prule.
+    bnds_datelist = list(rrule.rrule(prule, dtstart=parse(start_date), until=parse(end_date)))
+
+    # calculate the days since refdate, including refdate, with time being the
+    bnds_interval_since_refdate = cdftime.date2num(bnds_datelist)
+    if interval_type == "mid":
+        # mid-point value:
+        # time[n] = (bnds[n] + bnds[n+1]) / 2
+        time_interval_since_refdate = bnds_interval_since_refdate[0:-1] + np.diff(bnds_interval_since_refdate) / 2
+    elif interval_type == "start":
+        time_interval_since_refdate = bnds_interval_since_refdate[:-1]
+    else:
+        time_interval_since_refdate = bnds_interval_since_refdate[1:]
+
+    # create a new dimension for bounds only if it does not yet exist
+    time_dim = "time"
+    if time_dim not in list(nc.dimensions.keys()):
+        nc.createDimension(time_dim)
+
+    # create a new dimension for bounds only if it does not yet exist
+    bnds_dim = "nb2"
+    if bnds_dim not in list(nc.dimensions.keys()):
+        nc.createDimension(bnds_dim, 2)
+
+    # variable names consistent with PISM
+    time_var_name = "time"
+    bnds_var_name = "time_bnds"
+
+    # create time variable
+    time_var = nc.createVariable(time_var_name, "d", dimensions=(time_dim))
+    time_var[:] = time_interval_since_refdate
+    time_var.bounds = bnds_var_name
+    time_var.units = time_units
+    time_var.calendar = time_calendar
+    time_var.standard_name = time_var_name
+    time_var.axis = "T"
+
+    # create time bounds variable
+    time_bnds_var = nc.createVariable(bnds_var_name, "d", dimensions=(time_dim, bnds_dim))
+    time_bnds_var[:, 0] = bnds_interval_since_refdate[0:-1]
+    time_bnds_var[:, 1] = bnds_interval_since_refdate[1::]
+
+    nc.Conventions = "CF 1.5"
+    nc.close()
+
+
+def save_to_netcdf(filename, data):
+    """ Save results to a netcdf file"""
+
+    time = np.asarray([item["year"] for item in data])
+
+    start_year, end_year = time[0], time[-1]
+    create_netcdf(filename, start_date="{}-1-1".format(start_year), end_date="{}-1-1".format(end_year))
+
+    ne = len(data[0]["Y_gp"][0])
+
+    nc = NC(filename, "a")
+    nc.createDimension("ne", size=ne)
+    for idx, varname in enumerate(("limnsw", "limnsw_variance")):
+        var = nc.createVariable(varname, "d", dimensions=("time", "ne"), zlib=True, complevel=3)
+        var.units = "kg"
+        var[:] = np.squeeze(np.asarray([x["Y_gp"][idx] for x in data]))
+
+    nc.close()
+
+
+def set_size(w, h, ax=None):
+    """ w, h: width, height in inches """
+
+    if not ax:
+        ax = plt.gca()
+    l = ax.figure.subplotpars.left
+    r = ax.figure.subplotpars.right
+    t = ax.figure.subplotpars.top
+    b = ax.figure.subplotpars.bottom
+    figw = float(w) / (r - l)
+    figh = float(h) / (t - b)
+    ax.figure.set_size_inches(figw, figh)
+
+
 def emulate(year, X_new, samples_file, metadata):
 
     rcp = metadata["rcp"]
@@ -32,7 +153,7 @@ def emulate(year, X_new, samples_file, metadata):
 
     response_file = os.path.join(basedir, "dgmsl_rcp_{}_year_{}.csv").format(rcp, year)
 
-    print("Processing {}".format(response_file))
+    print("\nProcessing {}".format(response_file))
 
     # Load Samples file as Pandas DataFrame
     samples = pd.read_csv(samples_file, delimiter=",", squeeze=True, skipinitialspace=True)
@@ -55,10 +176,10 @@ def emulate(year, X_new, samples_file, metadata):
     # Dimension n of kernel
     n = X.shape[1]
 
-    # We choose a kernel
-    k = gp.kern.Exponential(input_dim=n, ARD=True)
+    # We choose an exponential kernel
+    kern = gp.kern.Exponential(input_dim=n, ARD=True)
 
-    m = gp.models.GPRegression(X, Y, k)
+    m = gp.models.GPRegression(X, Y, kern)
     m.optimize(messages=True)
 
     p = m.predict(X_new.values)
@@ -68,7 +189,7 @@ def emulate(year, X_new, samples_file, metadata):
     gp_df = pd.DataFrame(data=pctls_gp, index=[5, 16, 50, 84, 95], columns=["gp"])
     lhs_df = pd.DataFrame(data=pctls_lhs, index=[5, 16, 50, 84, 95], columns=["lhs"])
 
-    return {"year": year, "gp": gp_df, "lhs": lhs_df}
+    return {"year": year, "gp": gp_df, "lhs": lhs_df, "Y_gp": p, "Y_lhs": Y}
 
 
 def draw_samples(n_samples=1000):
@@ -167,13 +288,54 @@ if __name__ == "__main__":
         )
         pool.close()
 
-        print(results)
+    filename = "test.nc"
+    save_to_netcdf(filename, results)
 
-    gp = dict()
-    gp["years"] = np.asarray([item["year"] for item in results])
-    lhs = dict()
-    lhs["years"] = np.asarray([item["year"] for item in results])
+    # gp = dict()
+    # gp["years"] = np.asarray([item["year"] for item in results])
+    # lhs = dict()
+    # lhs["years"] = np.asarray([item["year"] for item in results])
 
-    for idx, pctl in enumerate(m_percentiles):
-        gp[pctl] = np.squeeze(np.asarray([item["gp"].values[idx, :] for item in results]).reshape(1, -1))
-        lhs[pctl] = np.squeeze(np.asarray([item["lhs"].values[idx, :] for item in results]).reshape(1, -1))
+    # for idx, pctl in enumerate(m_percentiles):
+    #     gp[pctl] = np.squeeze(np.asarray([item["gp"].values[idx, :] for item in results]).reshape(1, -1))
+    #     lhs[pctl] = np.squeeze(np.asarray([item["lhs"].values[idx, :] for item in results]).reshape(1, -1))
+
+    # # Because the above analysis can take some time,
+    # # here we want to take the results and save it in a file instead of making a plot right now.
+
+    # lw = 0.3
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.fill_between(gp["years"], gp[5], gp[95], color=rcp_col_dict[rcp], linewidth=lw, alpha=0.2)
+    # ax.fill_between(gp["years"], gp[16], gp[84], color=rcp_col_dict[rcp], linewidth=lw, alpha=0.2)
+    # for pctl in m_percentiles:
+    #     ax.plot(lhs["years"], lhs[pctl], color=rcp_col_dict[rcp], linewidth=0.3, linestyle=":")
+    # for pctl in m_percentiles:
+    #     ax.plot(gp["years"], gp[pctl], color=rcp_col_dict[rcp], linewidth=0.3, linestyle="-")
+    # ax.set_ylabel("Sea-level equivalent\n(cm)")
+    # ax.set_xlabel("Year")
+    # ax.set_xlim(lhs["years"].min(), lhs["years"].max())
+    # set_size(3.2, 1)
+    # fig.savefig("gp_rcp_{}.pdf".format(rcp), bbox_inches="tight", dpi=600)
+
+    # m_year = 2100
+    # Y_gp = results[-1]["Y_gp"][0]
+    # Y_lhs = results[-1]["Y_lhs"]
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.hist(
+    #     Y_lhs, bins=np.arange(int(Y_gp.min()), int(Y_gp.max()), 0.25), density=True, color=rcp_col_dict[rcp], alpha=0.4
+    # )
+    # ax.hist(
+    #     Y_gp,
+    #     bins=np.arange(int(Y_gp.min()), int(Y_gp.max()), 0.25),
+    #     histtype="step",
+    #     linewidth=0.6,
+    #     density=True,
+    #     color=rcp_col_dict[rcp],
+    #     alpha=1.0,
+    # )
+    # ax.set_xlabel("Sea-level equivalent (cm)")
+    # ax.set_ylabel("Density")
+    # set_size(3.2, 1.2)
+    # fig.savefig("gp_rcp_{}_hist_{}.png".format(rcp, m_year), bbox_inches="tight", dpi=600)
