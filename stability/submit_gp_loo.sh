a4be1b99e2af84e93ecab9034a86e69f9ef5aef8
Andy Aschwanden <aaschwanden@alaska.edu>
2019-07-17 13:34:18 -0800

    added script to submit files

---
+++ stability/submit_gp_loo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --partition=t2small
+#SBATCH --ntasks=28
+#SBATCH --tasks-per-node=28
+#SBATCH --time=6:00:00
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --output=gp-loo.%j
+
+
+cd $SLURM_SUBMIT_DIR
+
+ulimit -l unlimited
+ulimit -s unlimited
+ulimit
+
+module purge
+module load slurm
+module load lang/Python/3.5.2-pic-intel-2016b
+
+rcp=$1
+year=$2
+kernel=$3
+
+module purge
+module load slurm
+module load lang/Python/3.5.2-pic-intel-2016b
+
+python ../latin_hypercube/gp-loo.py -n 28 --rcp ${rcp} --year ${year} --kernel ${kernel} -s 2018_09_les/lhs_samples_gcm.csv 2018_09_les/dgmsl_csv/
