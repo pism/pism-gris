4abd9edc296689164d3775ab952342943e2f45b2
Andy Aschwanden <aaschwanden@alaska.edu>
2019-12-27 01:06:45 -0900

    clean up of nc2csv file

--- sobol/nc2csv.py
+++ sobol/nc2csv.py
@@ -14,31 +14,29 @@ parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.description = "Generating scripts for warming experiments."
 parser.add_argument("-v", "--variable", dest="variable", help="Variable to read in. Default=limnsw", default="limnsw")
 parser.add_argument("OUTFILE", nargs=1, help="Ouput file (CSV)", default=None)
-parser.add_argument("INFILES", nargs="*", help="Input file (netCDF)", default=None)
-parser.add_argument("-t", "--time_step", type=int, help="Time step to extract", default=-1)
+parser.add_argument("--indir", help="Base directory", default="2019_02_salt")
+parser.add_argument("-t", "--time_step", type=int, help="Time step to extract", default=92)
+parser.add_argument("-s", "--no_samples", dest="no_samples", type=int, help="No of Saltelli samples", default=400)
 
 options = parser.parse_args()
 variable = options.variable
 outfile = options.OUTFILE[0]
-# need to switch between the two solutions
-# infiles = list(braceexpand(options.INFILES[-1]))
-# infiles = options.INFILES
+no_samples = options.no_samples
 idx = options.time_step
 
-infiles = glob("2019_02_salt/dgmsl/dgmsl_*.nc")
-# print(infiles)
-
-ne = len(infiles)
-print(ne)
 data = np.zeros((2, 1))
 k = 0
-for infile in infiles:
+saltelli_multiplier = 13
+for m_id in range(0, no_samples * saltelli_multiplier):
+    infile = "{}/dgmsl/dgmsl_ts_gris_g1800m_v3a_rcp_45_id_{}_0_100.nc".format(options.indir, m_id)
     if os.path.isfile(infile):
         nc = NC(infile)
+        print(infile)
         if not variable in nc.variables:
             print("Variable {} not found, skipping".format(variable))
         else:
             id = int(re.search("id_(.+?)_", infile).group(1))
+            print(nc.variables[variable][92])
             val = nc.variables[variable][idx]
             units = nc.variables[variable].units
             if k == 0:
