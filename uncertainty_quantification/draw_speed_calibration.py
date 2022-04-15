c8a00e3a2acc676f158adc0a114931388e70d149
Andy Aschwanden <andy.aschwanden@gmail.com>
2022-04-15 07:33:35 -0800

    added random_seed to reproduce drawings

--- uncertainty_quantification/draw_speed_calibration.py
+++ uncertainty_quantification/draw_speed_calibration.py
@@ -19,7 +19,12 @@ from pyDOE import lhs
 parser = ArgumentParser()
 parser.description = "Draw samples using LHS or Saltelli."
 parser.add_argument(
-    "-s", "--n_samples", dest="n_samples", type=int, help="""number of samples to draw. default=10.""", default=10
+    "-s",
+    "--n_samples",
+    dest="n_samples",
+    type=int,
+    help="""number of samples to draw. default=10.""",
+    default=10,
 )
 parser.add_argument(
     "-m",
@@ -36,10 +41,15 @@ method = options.method
 n_samples = options.n_samples
 outfile = options.OUTFILE[-1]
 
+np.random.seed(2)
 
 distributions = {
-    "SIAE": uniform(loc=1.0, scale=3.0),  # uniform between 1 and 4    AS16 best value: 1.25
-    "SSAN": uniform(loc=3.0, scale=0.5),  # uniform between 3 and 3.5  AS16 best value: 3.25
+    "SIAE": uniform(
+        loc=1.0, scale=3.0
+    ),  # uniform between 1 and 4    AS16 best value: 1.25
+    "SSAN": uniform(
+        loc=3.0, scale=0.5
+    ),  # uniform between 3 and 3.5  AS16 best value: 3.25
     "PPQ": uniform(loc=0.25, scale=0.7),  # uniform between 0.25 and 0.95
     "TEFO": uniform(loc=0.005, scale=0.025),  # uniform between 0.015 and 0.030
     "PHIMIN": uniform(loc=5.0, scale=15.0),  # uniform between  5 and 20
